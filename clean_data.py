dbbca56202ba1580174c387802a4a3b4a2f28e40
darthvader99999 <bhow245@gmail.com>
2023-11-16 18:40:15 -0800

    removing unrelated stat columns

--- clean_data.py
+++ clean_data.py
@@ -1,9 +1,14 @@
 import sys
 import pandas as pd
-#splits the game logs by seasons and removes players who played < 20 games last season
+#splits the game logs by seasons and removes players who played < 20 games last season. Also removes unrelated stats
+
+columns = ['First Assists','Second Assists','IPP','Rebounds Created','PIM','Total Penalties','Minor','Major','Misconduct','Penalties Drawn','Giveaways','Takeaways','Hits',
+'Hits Taken','Shots Blocked','Faceoffs Won','Faceoffs Lost','Faceoffs %','Off. Zone Faceoffs','Neu. Zone Faceoffs','Def. Zone Faceoffs', 'Off. Zone Faceoff %',
+'CA','CF%','FA','FF%','SA','SF%','GA','GF%','xGA','xGF%','SCA','SCF%','SCGA','SCGF%','HDGA','HDGF%','On-Ice SV%','PDO','Neu. Zone Starts','Def. Zone Starts','On The Fly Starts','Off. Zone Start %']
 
 data = pd.read_pickle(sys.argv[1])
 data.index = data.index.set_levels([data.index.levels[0], pd.to_datetime(data.index.levels[1])])
+data = data.drop(columns = columns)
 last_season = data[data.index.get_level_values('Date') < "2023-6-20"]
 current_season = data[data.index.get_level_values('Date') > "2023-6-20"]
 

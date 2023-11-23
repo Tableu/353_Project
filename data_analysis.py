f38f266d68278acb3423966fa78ab847c0a93aa1
darthvader99999 <bhow245@gmail.com>
2023-11-22 22:22:43 -0800

    running analysis on all stats

--- data_analysis.py
+++ data_analysis.py
@@ -2,11 +2,10 @@ import sys
 import pandas as pd
 from scipy import stats
 import matplotlib.pyplot as plt
-if(len(sys.argv) != 3):
-    print("Expected input: data_file statistic")
+if(len(sys.argv) != 2):
+    print("Expected input: data_file")
     exit(1)
 data = pd.read_pickle(sys.argv[1])
-stat = sys.argv[2]
 avg_data = data.drop(columns = ['Game', 'Team'])
 avg_data = avg_data.rolling(3, closed='left').mean()
 avg_data = avg_data.dropna()
@@ -15,11 +14,20 @@ no_points_data = data[data['Total Points'] == 0]
 no_points_avg_data = avg_data.loc[no_points_data.index]
 has_points_data = data[data['Total Points'] > 0]
 has_points_avg_data = avg_data.loc[has_points_data.index]
-print('No Points '+stat+' Normal Test: '+str(stats.normaltest(no_points_avg_data[stat]).pvalue))
-print('Has Points '+stat+' Normal Test: '+str(stats.normaltest(has_points_avg_data[stat]).pvalue))
-print('Has/No Points '+stat+' variance test: '+str(stats.levene(no_points_avg_data[stat], has_points_avg_data[stat]).pvalue))
-plt.hist(no_points_avg_data[stat])
-plt.hist(has_points_avg_data[stat])
-plt.savefig('fig.png')
-ttest = stats.ttest_ind(no_points_avg_data[stat], has_points_avg_data[stat])
-print(stat+' T test: '+str(ttest.pvalue))
+
+tests = {'stat': [], 'has_points': [], 'no_points': [], 'variance': [], 'ttest': [], 'mannwhitneyu': []}
+for stat in list(avg_data.columns):
+    tests['stat'].append(stat)
+    tests['has_points'].append(stats.normaltest(no_points_avg_data[stat]).pvalue)
+    tests['no_points'].append(stats.normaltest(has_points_avg_data[stat]).pvalue)
+    tests['variance'].append(stats.levene(no_points_avg_data[stat], has_points_avg_data[stat]).pvalue)
+    plt.clf()
+    plt.hist(no_points_avg_data[stat])
+    plt.hist(has_points_avg_data[stat])
+    plt.savefig('plots/'+stat+'.png')
+    ttest = stats.ttest_ind(no_points_avg_data[stat], has_points_avg_data[stat])
+    mwu = stats.mannwhitneyu(no_points_avg_data[stat], has_points_avg_data[stat])
+    tests['ttest'].append(ttest.pvalue)
+    tests['mannwhitneyu'].append(mwu.pvalue)
+results = pd.DataFrame(data=tests)
+print(results)

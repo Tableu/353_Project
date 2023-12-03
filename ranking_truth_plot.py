1bbce12276f25801effb485ee14754a64b02b435
Raj Patel <rajhgsp@gmail.com>
2023-12-03 13:36:05 -0800

    fixed rankings and added prediction vs truth plot

--- ranking_truth_plot.py
+++ ranking_truth_plot.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-rankings = pd.read_csv('output/Player_rankings.csv')
-rankings = rankings.drop(rankings.index.to_list()[20:], axis=0)
-rankings['Actual_Shots'] = [2, 4, 3, 4, 2, 2, 2, 5, 8, 4, 2, 4, 2, 0, 2, 2, 0, 2, 3, 0]
-plt.plot(rankings['Actual_Shots'], rankings['Expected Shots'], 'b.', alpha=0.5)
+# rankings = pd.read_csv('output/Player_rankings.csv')
+# rankings = rankings.drop(rankings.index.to_list()[20:], axis=0)
+# rankings['Actual_Shots'] = [2, 4, 3, 4, 2, 2, 2, 5, 8, 4, 2, 4, 2, 0, 2, 2, 0, 2, 3, 0]
+rankings = pd.read_csv('output/ranking_analysis.csv')
+plt.plot(rankings['Truth'], rankings['Prediction'], 'b.', alpha=0.5)
 # plt.plot([3, 4, 5], [3, 4, 5], 'r-', linewidth=3)
 # plt.plot()
 plt.ylabel('Expected Shots')
